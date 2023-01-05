2558330282872f8862087c3ffd23acab82989806
anna8624 <anna8624@gmail.com>
2023-01-06 01:22:58 +0800

    Initial commit

--- bookstore.py
+++ bookstore.py
@@ -6,19 +6,18 @@ TODO:
     pip install webdriver-manager
     pip install webdriver-manager packaging
 """
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC #這行請手動輸入
 from selenium.webdriver.chrome.service import Service
-
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import json
 from datetime import datetime
-from webdriver_manager.chrome import ChromeDriverManager
 
 ops=Options()
 #ops.add_argument('--hesdless') #無頭模式，沒有視窗
